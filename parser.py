ecf048e66c1f04b502652af848facbec38f840d7
abhishek nagaraj <dalek2point3@gmail.com>
2014-10-25 16:48:01 -0400

    write multiple records per domain

--- parser.py
+++ parser.py
@@ -13,7 +13,41 @@ def strip_non_ascii(string):
     stripped = (c for c in string if 0 < ord(c) < 127)
     return ''.join(stripped)
 
+def makeline(items):
+    items = [strip_non_ascii(str(x)) for x in items]
+    items = [re.sub(r'[\n\t]+', '', x) for x in items]
+    row = "\t".join(items) + "\n"
+    return row
+
 def writedata(drecords, outfile = "data.csv"):
+
+    fields = ['city', 'country','name','email','organization','postalCode','state', 'street1', 'street2', 'telephone']
+    cols1 = ['domain', 'subrecord','created','updated','registrarname','registrarid','parsecode', 'contacttype']
+
+    cols = cols1 + fields
+
+    subrecords = ["whois", "registrydata"]
+    contacts = ['administrativeContact', 'billingContact','registrant','technicalContact','zoneContact']
+
+    with open(outfile, 'w') as outf:
+        outf.write(makeline(cols))
+
+        for d in drecords:
+            if d.domain != "":
+                for sr in subrecords:
+                    if getattr(d,sr) != "":
+                        sub_record = getattr(d,sr)
+                        items1 = [d.domain, sr, sub_record.createdDate, sub_record.updatedDate,sub_record.registrarName, sub_record.registrarIANAID, sub_record.parseCode]
+
+                        for c in contacts:
+                            cnt = getattr(sub_record, c)
+                            items2 = [c]
+                            for f in fields:
+                                items2.append(getattr(cnt,f))
+
+                            items = items1 + items2
+                            outf.write(makeline(items))
+
     return
 
 def get_data(domains):
@@ -25,7 +59,7 @@ def get_data(domains):
         d = DomainRecord(domain)
         d.get_data()
         d.parse_data()
-        domainrecords.append(d)
+        drecords.append(d)
     return drecords
 
 def test_record(d):
@@ -33,10 +67,10 @@ def test_record(d):
         print x, getattr(d.whois, x)
 
 if __name__ == "__main__":
-
     domains = readlist("domains.csv")
     drecords = get_data(domains)
-
+    writedata(drecords)
+    #test_record(drecords[0])
 
 
 
